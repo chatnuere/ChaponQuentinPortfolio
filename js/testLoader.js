abcc28288d6d99cfdbff5f90ad6bd0f9225bea9c
Quentin Chapon <q.chapon@gmail.com>
2015-01-05 16:53:34 +0100

    Test anims et menu ok

--- js/testLoader.js
+++ js/testLoader.js
@@ -1,27 +1,21 @@
 /**
  * Created by ChatNoir on 05/01/15.
  */
+var timeout
 initLoader = function(){
 
 };
 
 resetSquare = function(){
-
     $('.button').html('Eplode!!!!');
     $('#shrapnel').toggleClass('exploded');
-<<<<<<< HEAD
-=======
-    setTimeout(function(){ $('#shrapnel').css({'background-color': '#000'}); }, 1000)
->>>>>>> b7ec60b5d6e32acef456f4fd830443d33e53b238
+    timeout = setTimeout(function(){ $('#shrapnel').css({'background-color': '#000'}); }, 1000)
 };
 
 explodeSquare = function(){
+    clearTimeout(timeout);
     $('.button').html('Reset')
-<<<<<<< HEAD
-    $('#shrapnel').toggleClass('exploded');
-=======
     $('#shrapnel').toggleClass('exploded').css({'background-color': 'transparent'});
->>>>>>> b7ec60b5d6e32acef456f4fd830443d33e53b238
 };
 
 
