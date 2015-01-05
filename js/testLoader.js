12d573ff347722ad9b2941a0fa8343b2808fe0e3
chatnuere <dugue.pierrejean@gmail.com>
2015-01-05 16:19:45 +0100

    js bug correction

--- js/testLoader.js
+++ js/testLoader.js
@@ -1,18 +1,19 @@
 /**
  * Created by ChatNoir on 05/01/15.
  */
+var timeout
 initLoader = function(){
 
 };
 
 resetSquare = function(){
-
     $('.button').html('Eplode!!!!');
     $('#shrapnel').toggleClass('exploded');
-    setTimeout(function(){ $('#shrapnel').css({'background-color': '#000'}); }, 1000)
+    timeout = setTimeout(function(){ $('#shrapnel').css({'background-color': '#000'}); }, 1000)
 };
 
 explodeSquare = function(){
+    clearTimeout(timeout);
     $('.button').html('Reset')
     $('#shrapnel').toggleClass('exploded').css({'background-color': 'transparent'});
 };
