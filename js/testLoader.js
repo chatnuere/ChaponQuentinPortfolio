ddd524a129cf182f63d1abcd81bccc70da5e473f
chatnuere <dugue.pierrejean@gmail.com>
2015-01-09 11:47:48 +0100

    improving square

--- js/testLoader.js
+++ js/testLoader.js
@@ -7,15 +7,20 @@ initLoader = function(){
 };
 
 resetSquare = function(){
+    $('#shrapnel h1').fadeOut(200)
     $('.button').html('Eplode!!!!');
     $('#shrapnel').toggleClass('exploded');
+    $('.square').toggleClass('exploded')
     timeout = setTimeout(function(){ $('#shrapnel').css({'background-color': '#000'}); }, 1000)
+
 };
 
 explodeSquare = function(){
     clearTimeout(timeout);
     $('.button').html('Reset')
     $('#shrapnel').toggleClass('exploded').css({'background-color': 'transparent'});
+    $('#shrapnel h1').delay(400).fadeIn(600)
+    $('.square').toggleClass('exploded')
 };
 
 
@@ -23,6 +28,15 @@ explodeSquare = function(){
 $('.button').on('click', function(){
 
     $('#shrapnel').hasClass('exploded')? resetSquare() : explodeSquare()
+
+
+});
+
+$('.menuButton').on('click', function(){
+
+    $('#menu').toggleClass('expanded');
+    $('main').toggleClass('expanded');
+
 
 
 });
